46900a36e11151a37626dc30726bc588aeed0181
MdSharifhossain1234 <sharifhossainmd8@gmail.com>
2023-04-04 15:21:26 +0600

    first commit

---
+++ js/prompt.js
@@ -0,0 +1,16 @@
+// alert('how are you')
+const showAlert = () =>{
+    alert('dosto, taka ase dhar dite parbi,just 500?')
+}
+
+const lendMoney = () => {
+    const result = confirm('will you lend me 500tk')
+    console.log(result);
+}
+
+const getInfo = () => {
+    // alert
+    // confirm
+    const name = prompt('tell me your name')
+    console.log(name);
+}
